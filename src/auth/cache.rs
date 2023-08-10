b19ccd464c38887a469aa4dcf14924eff4f26e9c
Toru Ogawa <Hakuyume@users.noreply.github.com>
2023-08-10 10:30:36 +0900

    clippy

--- src/auth/cache.rs
+++ src/auth/cache.rs
@@ -15,13 +15,10 @@ impl Cache {
     #[tracing::instrument(ret, skip(self))]
     pub fn get(&self, scopes: &[&str], lifetime: Duration) -> Option<&Token> {
         let now = Utc::now();
-        if let Some(token) = self.map.get(
-            &scopes
-                .into_iter()
-                .copied()
-                .map(Box::from)
-                .collect::<Box<_>>(),
-        ) {
+        if let Some(token) = self
+            .map
+            .get(&scopes.iter().copied().map(Box::from).collect::<Box<_>>())
+        {
             (token.expires_at > now + lifetime).then_some(token)
         } else {
             None
@@ -31,11 +28,7 @@ impl Cache {
     #[tracing::instrument(skip(self))]
     pub fn put(&mut self, scopes: &[&str], token: Token) {
         self.map.insert(
-            scopes
-                .into_iter()
-                .copied()
-                .map(Box::from)
-                .collect::<Box<_>>(),
+            scopes.iter().copied().map(Box::from).collect::<Box<_>>(),
             token,
         );
         let now = Utc::now();
